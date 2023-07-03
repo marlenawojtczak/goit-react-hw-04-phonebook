563cbe1cc26a5a9c6247b46a624270e62a6fdebb
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-07-03 18:04:50 +0200

    added refactored code with hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,14 @@ import ContactList from '../ContactList/ContactList';
 import Filter from '../Filter/Filter';
 
 export const App = () => {
-  const initialState = () => [
+  const initialState = [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ];
 
-  const [contacts, setContacts] = useState(() => {
+  const [contacts, setContacts] = useState(zz => {
     return JSON.parse(localStorage.getItem('contacts')) ?? initialState;
   });
   const [filter, setFilter] = useState('');
